e9fc699fb85ea61df4abd8874dd408b5b02a1de8
tkd42 <42408602+tkd42@users.noreply.github.com>
2020-07-09 16:35:41 +0300

    Update Hello.cpp

--- hw1/Hello.cpp
+++ hw1/Hello.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main ( )
+int main(void)
 {
-    std::cout << "Hello Test" << std::endl;
-
+    std::cout <<"Hello Test"<<std::endl;
+    std::cout <<"Hello Test2"<<std::endl;
     return 0;
 }
